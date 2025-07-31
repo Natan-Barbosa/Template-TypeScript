e45956df78b3b0ab5f42ef63b291c531c37e1f27
natan <natanbarbosax193@gmail.com>
2025-07-31 16:49:24 -0300

    raw: add dotenv config

--- src/config/jet.logger.config.ts
+++ src/config/jet.logger.config.ts
@@ -1,7 +1,13 @@
 import { Formats, JetLogger, LoggerModes } from "jet-logger";
+import * as dotenv from "dotenv";
+import { rootPath } from "root.path.config";
+
+dotenv.config();
+const enviroment = process.env.NODE_ENV;
+dotenv.config({ path: `${rootPath}/EnvConfig/.env.${enviroment}` });
 
 export const logger = new JetLogger(
-  (process.env.JET_LOGGER_MODE as LoggerModes) || LoggerModes.File,
+  (process.env.JET_LOGGER_MODE as LoggerModes) || LoggerModes.Console,
   process.env.JET_LOGGER_FILEPATH || undefined,
   undefined,
   undefined,
