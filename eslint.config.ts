d707279190c010689586ab66948a3af798adcdd6
natan <natanbarbosax193@gmail.com>
2025-07-31 13:18:50 -0300

    build: eslint config file

---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "prefer-const": "error",
+      "no-const-assign": "error",
+      "no-duplicate-imports": "error",
+      "no-unused-vars": "warn",
+      camelcase: "error",
+      "default-case": "warn",
+      "no-console": "warn",
+      "no-empty": "error",
+      "no-empty-function": "error",
+      "no-var": "error",
+    },
+  },
+]);
