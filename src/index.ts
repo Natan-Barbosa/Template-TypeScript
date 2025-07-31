9b97be8b5111da223d6691c89cac7413fac7ac10
natan <natanbarbosax193@gmail.com>
2025-07-31 16:50:01 -0300

    feat: add config based in enviroment

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,27 @@ import express from "express";
 import morgan from "morgan";
 import { hello } from "@/services/test";
 import { logger } from "./config/jet.logger.config";
+import * as dotenv from "dotenv";
+import { enviroment } from "./constants/enviroment";
+
+dotenv.config();
+const envConfig = process.env.NODE_ENV || enviroment.dev;
 
 const app: Application = express();
 const port = 3000;
 
-app.use(morgan("common"));
+if (envConfig == enviroment.dev) {
+  app.use(morgan("common"));
+}
 
 app.get("/", (_req: Request, res: Response) => {
   res.send(hello());
 });
 
 app.listen(port, () => {
-  logger.imp(`Server Is Runing On Port: ${port}`);
+  if (envConfig == enviroment.dev) {
+    logger.imp(`Server Is Runing In Dev Mode On Port: ${port}`);
+  } else {
+    logger.imp(`Server Is Runing In Production Mode On Port: ${port}`);
+  }
 });
