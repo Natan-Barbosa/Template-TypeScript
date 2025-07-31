426e802e0a8b9a579f9214f82b5842805f6b1c1e
natan <natanbarbosax193@gmail.com>
2025-07-31 17:15:42 -0300

    fix: add enviroment dev case NODE ENV is not defined

--- src/config/jet.logger.config.ts
+++ src/config/jet.logger.config.ts
@@ -1,10 +1,11 @@
 import { Formats, JetLogger, LoggerModes } from "jet-logger";
 import * as dotenv from "dotenv";
 import { rootPath } from "root.path.config";
+import { enviroment } from "@/constants/enviroment";
 
 dotenv.config();
-const enviroment = process.env.NODE_ENV;
-dotenv.config({ path: `${rootPath}/EnvConfig/.env.${enviroment}` });
+const envConfig = process?.env?.NODE_ENV || enviroment.dev;
+dotenv.config({ path: `${rootPath}/EnvConfig/.env.${envConfig}` });
 
 export const logger = new JetLogger(
   (process.env.JET_LOGGER_MODE as LoggerModes) || LoggerModes.Console,
