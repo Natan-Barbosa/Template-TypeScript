25dbb72449113551222582056946059255e2a012
natan <natanbarbosax193@gmail.com>
2025-07-31 13:45:06 -0300

    feat: add morgan middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { Application, Request, Response } from "express";
+import express from "express";
+import morgan from "morgan";
+import { hello } from "@/services/test";
 
-const express = require("express");
 const app: Application = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.use(morgan("common"));
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send(hello());
 });
 
 app.listen(port, () => {
